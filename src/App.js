fc58a6b5ab8e00c94d25c4b046b30f5a21877c43
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-29 16:11:42 -0400

    installs redux and thunk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 
 export default class App extends React.Component {
-  componentDidMount() {
-    fetch("http://localhost:3000/api/v1/workouts")
-      .then((resp) => resp.json())
-      .then((data) => console.log(data));
-  }
-
   render() {
     return <div> Training Session</div>;
   }

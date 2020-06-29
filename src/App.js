f7f80d6f2a343d2ff410dd039062a7da292b81ca
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-29 17:04:41 -0400

    creates mapstatetoprop function to access what is already in store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 
-export default class App extends React.Component {
+class App extends React.Component {
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/workouts")
+      .then((resp) => resp.json())
+      .then((data) => console.log(data));
+  }
+
   render() {
     return <div> Training Session</div>;
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    accounts: state.workouts,
+  };
+};
+
+export default connect(mapStateToProps)(App);

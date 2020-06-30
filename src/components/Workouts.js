5b3ad2cc3f3d5708499eaca48aaf3ac3cb6cc054
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-29 20:47:33 -0400

    renders workout name and category to page

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Workouts = (props) => {
-  return <div>Workouts</div>;
+  return (
+    <div>
+      {props.workouts.map((workout) => (
+        <li>
+          {workout.name} - {workout.category}
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default Workouts;

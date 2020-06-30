a5f00e22816f0f6d39b86c5ae16cf7f0fa8c58b9
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-30 19:20:53 -0400

    adds value and state to make form controlled

--- src/components/WorkoutInput.js
+++ src/components/WorkoutInput.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 export default class WorkoutInput extends Component {
+  state = { name: "", category: "" };
+
   render() {
     return (
       <div>
         <form>
           <label>Workout Name: </label>
-          <input type="text" placeholder="Workout Name" />
-          <label> Workout Category </label>
+          <input
+            type="text"
+            placeholder="Workout Name"
+            value={this.state.name}
+          />
+          <br></br>
+          <label>Workout Category: </label>
           <input type="text" placeholder="Workout Category" />
         </form>
       </div>

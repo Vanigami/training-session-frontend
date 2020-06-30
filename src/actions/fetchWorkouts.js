a23205012c40ed0484cadfb11c3f582f7fcb3ce6
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-29 20:33:20 -0400

    updates fetch promise with dispatch function

--- src/actions/fetchWorkouts.js
+++ src/actions/fetchWorkouts.js
@@ -2,6 +2,11 @@ export function fetchWorkouts() {
   return (dispatch) => {
     fetch("http://localhost:3000/api/v1/workouts")
       .then((resp) => resp.json())
-      .then((data) => console.log(data));
+      .then((data) =>
+        dispatch({
+          type: "FETCH_WORKOUTS",
+          payload: data,
+        })
+      );
   };
 }

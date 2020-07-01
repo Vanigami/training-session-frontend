c7cd5a8c2b1567e9aad7cf64b1027194dfbcfa21
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-06-30 20:15:57 -0400

    adds handle event for values

--- src/components/WorkoutInput.js
+++ src/components/WorkoutInput.js
@@ -3,6 +3,12 @@ import React, { Component } from "react";
 export default class WorkoutInput extends Component {
   state = { name: "", category: "" };
 
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
@@ -12,10 +18,18 @@ export default class WorkoutInput extends Component {
             type="text"
             placeholder="Workout Name"
             value={this.state.name}
+            name="name"
+            onChange={this.handleChange}
           />
-          <br></br>
+          <br />
           <label>Workout Category: </label>
-          <input type="text" placeholder="Workout Category" />
+          <input
+            type="text"
+            placeholder="Workout Category"
+            value={this.state.category}
+            name="category"
+            onChange={this.handleChange}
+          />
         </form>
       </div>
     );

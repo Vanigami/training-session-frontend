29eb9589f2d1921ed8a9c5c71e9fc2ea1444028a
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-02 17:06:54 -0400

    renders specific workout id and category in workout route component

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -4,7 +4,11 @@ const Workout = (props) => {
   console.log(props);
   let workout = props.workouts[props.match.params.id - 1];
   console.log(workout);
-  return <li>{workout ? workout.name : null}</li>;
+  return (
+    <li>
+      {workout ? workout.name : null} - {workout ? workout.category : null}
+    </li>
+  );
 };
 
 export default Workout;

5a8176a7a27f8a3a08a70b0c68b4be491e94b2d5
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-02 16:23:43 -0400

    imports browserRouter and wraps around App/

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import workoutReducer from "./reducers/workoutReducer";
 
 import App from "./App";
@@ -16,7 +17,9 @@ let store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
 
   document.getElementById("root")

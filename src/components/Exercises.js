d6822c7985ad0ced890385d840295c6c68eb3c60
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-04 11:44:13 -0400

    sets up exercises component and imports to render in exercise container

---
+++ src/components/Exercises.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Exercises = (props) => {
+  return <div>Exercises</div>;
+};
+export default Exercises;
--- src/containers/ExercisesContainer.js
+++ src/containers/ExercisesContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ExerciseInput from "../components/ExerciseInput";
+import Exercises from "../components/Exercises";
 class ExercisesContainer extends React.Component {
   render() {
     return (
       <div>
         <ExerciseInput />
+        <Exercises />
       </div>
     );
   }

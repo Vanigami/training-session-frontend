77f1e04f9f6ad89294ab36d8934c6e80be2e3757
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-04 12:09:04 -0400

    renders workouts and exercises

--- src/containers/ExercisesContainer.js
+++ src/containers/ExercisesContainer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ExerciseInput from "../components/ExerciseInput";
 import Exercises from "../components/Exercises";
+
 class ExercisesContainer extends React.Component {
   render() {
     return (
       <div>
-        <ExerciseInput />
-        <Exercises />
+        <ExerciseInput workout={this.props.workout} />
+        <br />
+        <Exercises
+          exercises={this.props.workout && this.props.workout.exercises}
+        />
       </div>
     );
   }

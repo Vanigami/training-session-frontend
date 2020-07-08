b49ce3f112f3c3e154122ca38cedcf72971d3bc1
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-08 10:36:11 -0400

    changes workout variable to filter through workouts array and match id

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -3,11 +3,11 @@ import { Redirect } from "react-router-dom";
 import ExercisesContainer from "../containers/ExercisesContainer";
 
 const Workout = (props) => {
-  let workout = props.workouts[props.match.params.id - 1];
-  // let workout = props.workouts.filter(
-  //   (workout) => workout.id == props.match.params.id
-  // )[0];
-  // console.log(workout);
+  // let workout = props.workouts[props.match.params.id - 1];
+  let workout = props.workouts.filter(
+    (workout) => workout.id == props.match.params.id
+  )[0];
+  console.log(workout);
   return (
     <div>
       <h2>

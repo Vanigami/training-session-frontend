c04d1c596214ef6bf27db2669badbe2b0d8c5095
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-08 11:38:50 -0400

    adds exercise state and handler

--- src/components/ExerciseInput.js
+++ src/components/ExerciseInput.js
@@ -2,18 +2,42 @@ import React from "react";
 import { connect } from "react-redux";
 
 class ExerciseInput extends React.Component {
+  state = {
+    equipment: "",
+    exercise: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
         <form>
           <label>equipment:</label>
-          <select>
+          <select
+            name="equipment"
+            value={this.state.equipment}
+            onChange={this.handleChange}
+          >
             <option>none</option>
             <option>dumbells</option>
             <option>barbell</option>
             <option>kettlebell</option>
             <option>resistance bands</option>
           </select>
+          <br />
+          <label>Exercise Name:</label>
+          <input
+            type="text"
+            name="exercise"
+            value={this.state.exercise}
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
         </form>
       </div>
     );

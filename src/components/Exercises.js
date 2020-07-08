b1bcfdd9743b7433d6e0c3fb432a9436f06f789f
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-08 10:32:10 -0400

    passes exercises props to render exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 const Exercises = (props) => {
-  return <div>Exercises</div>;
+  return (
+    <div>
+      {props.exercises &&
+        props.exercises.map((exercise) => (
+          <li key={exercise.id}>
+            - {exercise.exercise} <br />
+            equipment needed: {exercise.equipment}
+          </li>
+        ))}
+    </div>
+  );
 };
 export default Exercises;

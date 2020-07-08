c5a1a333ca70c6fe2e2817fa38ce755e1c71415f
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-08 19:59:36 -0400

    sets up edit workout dispatch request and returns json workout object

---
+++ src/actions/editWorkout.js
@@ -0,0 +1,14 @@
+export const editWorkout = (data) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/workouts/${data.id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "PATCH",
+      body: JSON.stringify(data),
+    })
+      .then((resp) => resp.json())
+      .then((workout) => dispatch({ type: "EDIT_WORKOUT", payload: workout }));
+  };
+};

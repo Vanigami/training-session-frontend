fe03089fb062e46b3c2dae075d1c0260c2fbc8cc
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-07-11 00:15:40 -0400

    adds sort button

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -8,26 +8,15 @@ const Workouts = (props) => {
   return (
     <div>
       <div className="workout">
-        {props.workouts
-          .sort((b, a) => b.id - a.id)
-          .map((workout) => (
-            <li key={workout.id}>
-              <Link to={`/workouts/${workout.id}`}>
-                {workout.name} - {workout.category}
-              </Link>
-              {/* <img src="https://static.vecteezy.com/system/resources/thumbnails/000/539/724/small/dumbbell_2__28b_w_29.jpg" /> */}
-            </li>
-          ))}
+        {props.workouts.map((workout) => (
+          <li key={workout.id}>
+            <Link to={`/workouts/${workout.id}`}>
+              {workout.name} - {workout.category}
+            </Link>
+          </li>
+        ))}
+        <button>Sort</button>
       </div>
-
-      {props.workout &&
-        props.workouts.sort().map((workout) => {
-          if (this.workout.category == "Endurance") {
-            {
-              return "sorted";
-            }
-          }
-        })}
     </div>
   );
 };

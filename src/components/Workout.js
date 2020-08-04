0bd3d553b54fc74de7d05f2e8a585fa1088ad58d
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-08-04 16:43:04 -0400

    adds sort button that alphabetizes workouts

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,36 +1,25 @@
 import React from "react";
 // import { Redirect } from "react-router-dom";
 import WorkoutEdit from "./WorkoutEdit";
-
 import ExercisesContainer from "../containers/ExercisesContainer";
 
-class Workout extends React.Component {
-  // state = {
-  //   count: 0,
-  // };
-
-  // increment = () => {
-  //   this.setState({ counter: this.state.counter + 1 },);
-  // };
+const Workout = (props) => {
   // let workout = props.workouts[props.match.params.id - 1];
-  workout = this.props.workouts.filter(
-    (workout) => workout.id == this.props.match.params.id
+  let workout = props.workouts.filter(
+    (workout) => workout.id == props.match.params.id
   )[0];
-
-  render() {
-    return (
-      <div>
-        <h2>
-          {this.workout ? this.workout.name : null} -{" "}
-          {this.workout ? this.workout.category : null}
-        </h2>
-        <ExercisesContainer workout={this.workout} />
-        <br />
-        <h4>Edit Workout</h4>
-        <WorkoutEdit workout={this.workout} />
-      </div>
-    );
-  }
-}
+  console.log(workout);
+  return (
+    <div>
+      <h2>
+        {workout ? workout.name : null} - {workout ? workout.category : null}
+      </h2>
+      <ExercisesContainer workout={workout} />
+      <br />
+      <h4>Edit Workout</h4>
+      <WorkoutEdit workout={workout} />
+    </div>
+  );
+};
 
 export default Workout;
--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,35 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Link } from "react-router-dom";
 import Workout from "./Workout";
 import Counter from "./Counter";
 import "../App.css";
 
-class Workouts extends React.Component {
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
+const Workouts = (props) => {
+  const [toggle, setToggle] = useState(false);
+
+  const sortByAlph = () => {
+    console.log(toggle);
+    if (toggle) {
+      return [...props.workouts].sort((a, b) => (a.name > b.name ? 1 : -1));
+    } else {
+      return props.workouts;
+    }
   };
 
-  render() {
-    return (
-      <>
-        <div onSubmit={this.handleSubmit}>
-          <div class="card">
-            {this.props.workouts.map((workout) => (
-              <li class="container" key={workout.id}>
-                <h3>
-                  <Link to={`/workouts/${workout.id}`}>
-                    {workout.name} <br />
-                  </Link>
-                  {workout.category}
-                  {<Counter />}
-                </h3>
-              </li>
-            ))}
-          </div>
+  const handleToggledClick = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <>
+      <div onSubmit={props.handleSubmit}>
+        <label>{toggle}</label>
+        <div class="card">
+          <button onClick={handleToggledClick}>Alphabetize!</button>
+
+          {sortByAlph().map((workout) => (
+            <li class="container" key={workout.id} value={workout.id}>
+              <h3>
+                <Link to={`/workouts/${workout.id}`}>
+                  {workout.name} <br />
+                </Link>
+                {workout.category}
+
+                {/* <Counter /> */}
+              </h3>
+            </li>
+          ))}
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
 export default Workouts;
